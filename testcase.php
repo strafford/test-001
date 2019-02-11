e0eac7bdeee81695e2f861a63b543773aca92607
AnrDaemon <anrdaemon@yandex.ru>
2019-02-12 01:37:38 +0300

    Block function quick test.

--- testcase.php
+++ testcase.php
@@ -2,8 +2,22 @@
 
 require_once __DIR__ . '/smarty-config.php';
 
+function Zsmarty_block_t ($params, $content, Smarty_Internal_Template $template, &$repeat)
+{
+  if (isset($content)) {
+    return "blah";
+  }
+}
+
+$smarty->registerPlugin('block', 'tz', 'Zsmarty_block_t');
+
 $smarty->display(__FILE__);
 
 __HALT_COMPILER();
-?>PHP: {$smarty.const.PHP_VERSION}
+?>Hello
+This is not translated
+{t}This is translated{/t} 
+{tz}This is z translated{/tz} 
+
+PHP: {$smarty.const.PHP_VERSION}
 Smarty: {$smarty.version}
